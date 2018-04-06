ad659ea60a53c08daaa1e8927c63068c571c0777
progfay <shun1856shun@gmail.com>
2018-04-07 00:03:39 +0900

    feat(hammer): fix no event fire with gesture
    - react-hammerjs: https://github.com/cmilfont/react-hammerjs/blob/master/src/Hammer.js
    - change detected gesture from pan to swipe
      - if you wanna know difference between pan and swipe, see below website
        - https://gist.github.com/hrdtbs/948befbc7810d9f58c917a969c562212

--- components/withPanGesture.js
+++ components/withPanGesture.js
@@ -3,14 +3,36 @@ import { withRouter } from 'next/router';
 
 export default withRouter(
   ({ children, router, up, down, left, right }) => (
-    <Hammer onPan={router.push(up)} direction='DIRECTION_UP'>
-    <Hammer onPan={router.push(down)} direction='DIRECTION_DOWN'>
-      <Hammer onPan={router.push(left)} direction='DIRECTION_LEFT'>
-        <Hammer onPan={router.push(right)} direction='DIRECTION_RIGHT'>
+    <React.Fragment>
+      <Hammer direction='DIRECTION_ALL' onSwipe={(e) => {
+        switch(e.direction) {
+          case 16:
+            router.push(up);
+            break;
+          case 8:
+            router.push(down);
+            break;
+          case 4:
+            router.push(left);
+            break;
+          case 2:
+            router.push(right);
+            break;
+        }
+      }}>
+        <div className='hammer'>
           {children}
-        </Hammer>
+        </div>
       </Hammer>
-    </Hammer>
-  </Hammer>
+      <style jsx>{`
+      .hammer {
+          position: fixed;
+          top: 0;
+          left: 0;
+          width: 100vw;
+          height: 100vh;
+        }
+      `}</style>
+    </React.Fragment>
   )
 );
