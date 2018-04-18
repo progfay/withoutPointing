80a9a54610304464e1cb381192df4dfd60be3851
progfay <shun1856shun@gmail.com>
2018-04-18 19:25:35 +0900

    feat(content): bundle withPanGesture and sideLink

---
+++ components/content.js
@@ -0,0 +1,36 @@
+import WithPanGesture from '../components/withPanGesture';
+import { TopSlideLink, BottomSlideLink, LeftSlideLink, RightSlideLink } from '../components/slideLink';
+
+export default ({ up, down, left, right, children }) => (
+    <React.Fragment>
+      <WithPanGesture up={up.link} down={down.link} left={left.link} right={right.link}>
+        <TopSlideLink icon={up.icon} />
+        <BottomSlideLink icon={down.icon} />
+        <LeftSlideLink icon={left.icon} />
+        <RightSlideLink icon={right.icon} />
+        <div className='content-outer'>
+          <div className="content-inner">
+            {children}
+          </div>
+        </div>
+    </WithPanGesture>
+
+    <style jsx>{`
+      .content-outer {
+        position: fixed;
+        top: 10vmin;
+        left: 10vmin;
+        width: calc(100vw - 20vmin);
+        height: calc(100vh - 20vmin);
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        z-index: 0;
+      }
+
+      .content-inner {
+        text-align: center;
+      }
+    `}</style>
+  </React.Fragment>
+)
