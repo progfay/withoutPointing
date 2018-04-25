a0c5fb51ec9bc2ac6a6385c26df07335236674de
progfay <shun1856shun@gmail.com>
2018-04-25 19:28:57 +0900

    feat(slideLink): change to set at least only 1 icon

--- components/slideLink.js
+++ components/slideLink.js
@@ -1,11 +1,37 @@
 import SlideStyle from '../style/slideStyle';
 
-const TopSlideLink = ({ children }) => <SlideStyle.Top> {children} </SlideStyle.Top>;
+import FontAwesomeIcon    from '@fortawesome/react-fontawesome';
+import faAngleDoubleUp    from '@fortawesome/fontawesome-free-solid/faAngleDoubleUp'
+import faAngleDoubleDown  from '@fortawesome/fontawesome-free-solid/faAngleDoubleDown'
+import faAngleDoubleLeft  from '@fortawesome/fontawesome-free-solid/faAngleDoubleLeft'
+import faAngleDoubleRight from '@fortawesome/fontawesome-free-solid/faAngleDoubleRight'
 
-const BottomSlideLink = ({ children }) => <SlideStyle.Bottom> {children} </SlideStyle.Bottom>;
+const TopSlideLink = ({ icon }) => (
+  <SlideStyle.Top>
+    <FontAwesomeIcon icon={faAngleDoubleUp} />
+    <FontAwesomeIcon icon={icon} />
+  </SlideStyle.Top>
+);
 
-const LeftSlideLink = ({ children }) => <SlideStyle.Left> {children} </SlideStyle.Left>;
+const BottomSlideLink = ({ icon }) => (
+  <SlideStyle.Bottom>
+    <FontAwesomeIcon icon={faAngleDoubleDown} />
+    <FontAwesomeIcon icon={icon} />
+  </SlideStyle.Bottom>
+);
 
-const RightSlideLink = ({ children }) => <SlideStyle.Right> {children} </SlideStyle.Right>;
+const LeftSlideLink = ({ icon }) => (
+  <SlideStyle.Left>
+    <FontAwesomeIcon icon={faAngleDoubleLeft} />
+    <FontAwesomeIcon icon={icon} />
+  </SlideStyle.Left>
+);
+
+const RightSlideLink  = ({ icon }) => (
+  <SlideStyle.Right>
+    <FontAwesomeIcon icon={faAngleDoubleRight} />
+    <FontAwesomeIcon icon={icon} />
+  </SlideStyle.Right>
+);
 
 export { TopSlideLink, BottomSlideLink, LeftSlideLink, RightSlideLink };
